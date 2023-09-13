aaf28dc259c5b822aaf22bbc354e5b67b192ec53
Fabricio Alves <fabricioallves2020@gmail.com>
2023-09-13 15:29:45 -0300

    tela de cadastro feita

---
+++ dailytaskes/src/Components/Card/Card.jsx
@@ -0,0 +1,97 @@
+import { Section, ErroSpan } from "./CardStyled";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userCreate } from "../../services/UserServices";
+import { useState, useEffect } from "react";
+
+const registerSchema = z.object({
+    name: z.string().nonempty({ message: "Nome não pode ser vazio" }).refine(value => !/^\s*$/.test(value), { message: "Nome não pode ter apenas espaços" }),
+    username: z.string().nonempty({ message: "Sobrenome não pode ser vazio" }).refine(value => !/^\s*$/.test(value), { message: "Sobrenome não pode ter apenas espaços" }),
+    email: z.string().nonempty({ message: "Email não pode ser vazio" }).refine((value) => /@/.test(value), { message: "Email deve conter um @" }),
+    password: z.string().nonempty({ message: "Senha não pode ser vazia" }).refine((value) => /^(?=.*[@!#$%^&*()/\\])[@!#$%^&*()/\\a-zA-Z0-9]{8,12}$/.test(value), { message: "Senha deve estar entre 8 e 12 caracters e dev conter ao menos um caracter especial" })
+})
+
+export function Card() {
+
+    const [userData, setData] = useState({});
+    const navigate = useNavigate();
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({
+        resolver: zodResolver(registerSchema)
+    })
+
+    async function onRegister(data) {
+        const { name, username, email, password } = data;
+        const response = await userCreate({name, username, email, password });
+        navigate("/");
+        reset();
+    }
+
+
+    return (
+        <Section>
+            <form onSubmit={handleSubmit(onRegister)}>
+                <label>
+                    Nome: <br />
+                    <input type="text" {...register("name")} placeholder="Insira o nome" />
+                    {errors.name && <ErroSpan>{errors.name.message}</ErroSpan>}
+                </label>
+                <label >
+                    Sobrenome:
+                    <br />
+                    <input type="text" {...register("username")} placeholder="Insira o sobrenome" />
+                    {errors.username && <ErroSpan>{errors.username.message}</ErroSpan>}
+                </label>
+                <label >
+                    E-mail:
+                    <br />
+                    <input type="text" {...register("email")} placeholder="Insira o Email" />
+                    {errors.email && <ErroSpan>{errors.email.message}</ErroSpan>}
+                </label>
+                <label >
+                    Senha:
+                    <br />
+                    <input type="text" {...register("password")} placeholder="Insira uma senha" />
+                    {errors.password && <ErroSpan>{errors.password.message}</ErroSpan>}
+                </label>
+                <div>
+                    <button>Finalizar Cadastro</button>
+                </div>
+            </form>
+        </Section>
+    )
+}
+
+export function CardLogin() {
+    return (
+        <Section top>
+            <form>
+                <label>
+                    Email:
+                    <br />
+                    <input type="text" name="email" placeholder="Insira o email" />
+                </label>
+                <label>
+                    Senha:
+                    <br />
+                    <input type="text" name="password" placeholder="Insira a senha" />
+                </label>
+                <div>
+                    <button>
+                        Login
+                    </button>
+                </div>
+                <section>
+                    <div>
+                        <a href="#">Ainda não é cadastrado? Clique Aqui</a>
+                    </div>
+                    <div>
+                        <a href="#">Esqueceu a senha? clique aqui</a>
+                    </div>
+                </section>
+            </form>
+        </Section>
+    )
+}
--- dailytaskes/src/main.jsx
+++ dailytaskes/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import do estilo global
+import { StyleGlobal } from './StyleGlobal.jsx';
+// import do modulo de rotas od react-rout-dom
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Navbar } from './Components/Navbar/Navbar.jsx';
+import { Card, CardLogin } from './Components/Card/Card.jsx';
+import ErrorPage from "./Pages/ErrorPage/ErrorPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+    errorElement: <ErrorPage />,
+    children: [{
+      path: "/",
+      element: <CardLogin />
+    },
+    {
+      path: "/Cadastrar",
+      element: <Card />
+    }]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StyleGlobal></StyleGlobal>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
